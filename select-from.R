bf22ce323700089c4cff2e045ddd50106bf6df86
Ken Butler <ken.butler@utoronto.ca>
2024-03-01 12:31:12 -0500

    edit select-from

--- select-from.R
+++ select-from.R
@@ -3,6 +3,6 @@ desc <- read_csv("descriptions.csv")
 desc
 
 desc %>%
-  filter(str_detect(technique, "sign test")) %>%
+  filter(str_detect(technique, "survival")) %>%
   select(dataset, technique) %>%
   slice_sample(n = 10)
