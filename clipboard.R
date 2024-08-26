57405837b0f59b0a6d3108c5d79c348eb46ad842
Ken Butler <ken.butler@utoronto.ca>
2024-08-26 14:11:11 -0400

    more datasets plus edit to clipboard

--- clipboard.R
+++ clipboard.R
@@ -1,3 +1,5 @@
+# idea from https://stackoverflow.com/questions/10959521/how-to-write-to-clipboard-on-ubuntu-linux-in-r
+
 clipboard <- function(x, sep = "\t") {
   con <- pipe("xclip -selection clipboard -i", open = "w")
   write(x, con, sep = sep)
